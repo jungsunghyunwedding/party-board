93b89eac39e7a2eb9ab43c72cfe9360b0800f62c
jungsunghyunwedding <limhasic7360@gmail.com>
2026-08-20 16:15:37 +0900

    Add Supabase config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: "https://zadwtdoehzkvivuodwpd.supabase.co",
+  anonKey: "sb_publishable_ao4AgXWSJlkcDQMQCSaOOA_FaH3-0AI"
+};
+// 입장 비밀번호는 DB app_settings.room_password_hash 로 검증 (기본값: party)
